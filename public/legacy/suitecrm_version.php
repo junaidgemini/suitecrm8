b4d0404d6c8b3b4901c993b083359ed4651ff47d
Muhammad Junaid <junaid.gemini24@gmail.com>
2022-02-06 22:51:08 +0500

    change for gitignore

---
+++ public/legacy/suitecrm_version.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$suitecrm_version = '8.0.3';
+$suitecrm_legacy = '7.12.3';
+$suitecrm_timestamp = '2022-01-28 12:00:00';
